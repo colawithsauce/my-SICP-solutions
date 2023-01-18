c3a574d716baf10890fd4b8341ad402fd25a42a8
cola_with_sauce <cola_with_sauce@foxmail.com>
2023-01-18 22:26:03 +0800

    Finish Exercise 2.31

--- Chapter2/exercise30.rkt
+++ Chapter2/exercise30.rkt
@@ -20,3 +20,33 @@
        tree))
 
 ;; Exercise 2.31
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map proc sub-tree)
+             (proc sub-tree)))
+       tree))
+
+;; Exercise 2.32 - Generate the set of subset of a set.
+;; (define (subsets s)
+;;   (if (null? s)
+;;       (list nil)
+;;       (let ((rest (subsets (cdr s))))
+;;         (append rest (map subsets rest)))))
+
+;; Explain why it works:
+;; - Because if we assume the procedure `(map subsets rest)' would yield correct answer that "the set of the set of subset of the set rest"
+;; -- Because it can't works !!! XP
+
+(define (subsets s)
+  (if (null? s)
+      (list nil)
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x) (cons (car s) x))
+                          rest)))))
+
+(display (subsets '(1 2 3)))
+
+;; Explain why it works: - Because the subsets of a set is equal to "the subsets
+;; of its subset that only excludes its first element" (let's call it rest)
+;; append with "rest" in which each subset append the original first element.
