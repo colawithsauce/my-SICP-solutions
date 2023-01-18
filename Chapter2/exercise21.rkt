294d1cb9dc6a5ceca1e2f7874ac4c6516270f8df
cola_with_sauce <cola_with_sauce@foxmail.com>
2023-01-18 21:30:28 +0800

    Finish Exercise 2.29

--- Chapter2/exercise21.rkt
+++ Chapter2/exercise21.rkt
@@ -93,3 +93,41 @@
 
 (display (fringe (list (list 1 2) (list 3 4 (list 5 (list 6))))))
 
+;; Exercise 2.29
+(define (make-mobile left right)
+  (list left right))
+(define (make-branch length structure)
+  (list length structure))
+
+;; a - define selectors for the upper two data structures.
+(define (left-branch mobile) (car mobile))
+(define (right-branch mobile) (cadr mobile))
+(define (branch-length branch) (car branch))
+(define (branch-structure branch) (cadr branch))
+
+;; b - define `total-weight'
+(define (total-weight mobile)
+  (define (branch-weight branch)
+    ;; The weight of branch is the weight of its structure.
+    (let ((structure (branch-structure branch)))
+      (if (number? structure)
+          structure
+          (total-weight structure))))
+  ;; The weight of mobile is the weight of left branch plus wight of right branch
+  (+ (branch-weight (left-branch mobile))
+     (branch-weight (right-branch mobile))))
+
+;; c - design `balance?' to test if mobile is balanced.
+(define (balance? mobile)
+  (let ((left-struc (branch-structure (left-branch mobile)))
+        (left-length (branch-length (left-branch mobile)))
+        (right-struc (branch-structure (right-branch mobile)))
+        (right-length (branch-length (right-branch mobile))))
+    (if (number? mobile)
+        #t
+        (and (= (* left-struc left-length)
+                (* right-struc right-length))
+             (balance? left-struc)
+             (balance? right-struc)))))
+
+;; d - If we change the getter, we would only needs change setter.
