3d703b6ed4f21a87de70cd745eeb49cbf1b24969
cola_with_sauce <cola_with_sauce@foxmail.com>
2023-01-21 16:42:21 +0800

    Amend 2.43

--- Chapter2/exercise40.rkt
+++ Chapter2/exercise40.rkt
@@ -138,3 +138,17 @@
 ;; (length (queens 8))
 
 ;; Exercise 2.43 - Explain the low speed of Louis's procedure.
+;;
+;; NOTE: Resort to answers, thanks @jpath at http://community.schemewiki.org/?sicp-ex-2.43.
+;;
+;; Exchanging the order of the mapping in the flatmap results in queen-cols
+;; being re-evaluated for every item in (enumerate-interval 1 board-size).
+;; Therefore the whole work has to be duplicated board-size times at every
+;; recursion level. Since there are always board-size recursions this means that
+;; the whole work will be duplicated bord-size^board-size times.
+;;
+;; Therefore if the function would take T time to run for board-size=8 with
+;; correct ordering, with the interchanged ordering it will take (8^8)T to run.
+;;
+;; So we can draw a priciple here: When cascading `map' and `flatmap', the time
+;; consuming procedures should be positioned in the outer.
