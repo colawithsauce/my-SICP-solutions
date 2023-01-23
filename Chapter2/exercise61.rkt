ba134d0fcb70b45ab1a0518af2e456231f57cd25
cola_with_sauce <cola_with_sauce@foxmail.com>
2023-01-23 10:04:31 +0800

    Finish 2.62

--- Chapter2/exercise61.rkt
+++ Chapter2/exercise61.rkt
@@ -29,3 +29,15 @@
         ((= x (car set)) set)
         ((< x (car set)) (cons x set))
         ((> x (car set)) (cons (car set) (adjoin-set x (cdr set))))))
+
+;; Exercise 2.62 - `union-set' of ordered set. Complexity should be O(n)
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+         (let ((x1 (car set1))
+               (x2 (car set2)))
+           (cond ((= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2))))
+                 ((< x1 x2) (cons x1 (union-set (cdr set1) set2)))
+                 ((> x1 x2) (cons x2 (union-set set1 (cdr set2)))))))))
