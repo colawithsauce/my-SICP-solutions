6b0a26bccdab967ef1feaf4468609fb39c6ab859
cola_with_sauce <cola_with_sauce@foxmail.com>
2023-01-23 09:55:07 +0800

    Finish 2.61

---
+++ Chapter2/exercise61.rkt
@@ -0,0 +1,31 @@
+#lang sicp
+;;; Sets as orderd list
+
+;; Assume that the list is ordered increasingly.
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1))
+            (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1 (intersection-set (cdr set1)
+                                          (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((> x1 x2)
+               (intersection-set set1 (cdr set2)))))))
+
+;; Exercise 2.61 - `adjoin-set' of ordered set.
+;; The hard part is, if we access elements of set in recursive style,
+;; the return value should become the subset of the original set.
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        ((> x (car set)) (cons (car set) (adjoin-set x (cdr set))))))
