ca62e4c078a47ec864aa9e365fabeedc1cc26c98
cola_with_sauce <cola_with_sauce@foxmail.com>
2023-01-28 20:07:44 +0800

    2.73 - 2.77 skipped

---
+++ Chapter2/exercise73.rkt
@@ -0,0 +1,9 @@
+#lang sicp
+;; 2.73 - Rewrite symbolic deriv in apply-generic style.
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp))
+               (operands exp) var))))
+
+;; 2.73 - 2.77 skipped
