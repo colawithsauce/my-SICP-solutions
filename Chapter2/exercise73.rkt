50186d8fa44e8f9dc9f3c2cf25d801fd1e12e55a
cola_with_sauce <cola_with_sauce@foxmail.com>
2023-01-29 11:24:59 +0800

    2.80

--- Chapter2/exercise73.rkt
+++ Chapter2/exercise73.rkt
@@ -1,9 +1,10 @@
 #lang sicp
 ;; 2.73 - Rewrite symbolic deriv in apply-generic style.
-(define (deriv exp var)
-  (cond ((number? exp) 0)
-        ((variable? exp) (if (same-variable? exp var) 1 0))
-        (else ((get 'deriv (operator exp))
-               (operands exp) var))))
+
+;; (define (deriv exp var)
+;;   (cond ((number? exp) 0)
+;;         ((variable? exp) (if (same-variable? exp var) 1 0))
+;;         (else ((get 'deriv (operator exp))
+;;                (operands exp) var))))
 
 ;; 2.73 - 2.77 skipped
